08598af421ca8c88e4322f9d3bfc209c830ab235
HUSSEIN ABDO <125362049+BomBa-001@users.noreply.github.com>
2023-12-07 16:26:48 +0000

    001

--- app.vue
+++ app.vue
@@ -1,13 +1,58 @@
 <template>
-  <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
-        </div>
-      </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
-    </UCard>
-  </UContainer>
+  <div
+    v-if="isLoading"
+    class="mt-20 w-full flex items-center justify-center mx-auto"
+  >
+    <div class="text-white mx-auto flex flex-col items-center justify-center">
+      <Icon name="eos-icons:bubble-loading" size="48" color="#f00" />
+      <h1>{{ conterLoad }}</h1>
+      <div class="w-full mt-1">Loading...</div>
+    </div>
+  </div>
+
+  <!-- icon py nuxt-icon:- 
+    <Icon name="eos-icons:bubble-loading" size="48" color="#f00" />
+  -->
+  <!-- icon py ui:-
+    <UIcon name="i-heroicons-home" />
+  -->
+
+  <br />
+  <hr />
+  <br />
+
+  <UButton loading>Button</UButton>
+
+  <VitePwaManifest />
+  <NuxtPage />
 </template>
+
+<script setup lang="ts">
+// #region {import} --- --- --- ---
+import { ref, onMounted } from "vue";
+// #endregion --- --- --- --- ---
+
+// #region {data} --- --- --- ---
+let isLoading = ref(true);
+let conterLoad = ref(0);
+// #endregion --- --- --- --- ---
+
+// #region {func} --- --- --- ---
+onMounted(async () => {
+  await delay((e) => conterLoad.value++, 5000);
+
+  isLoading.value = false;
+});
+
+// const func_load = async (e) => {
+//   return await promise((res) => setTimeout(() => {
+
+//   }, 5000));
+// };
+// const sleepFunc = (function:func,sleepTime=1000) =>
+//   promise((f) => setTimeout(() =>f, sleepTime));
+
+const delay = (func: f, ms: number) => new Promise(() => setTimeout(f, ms));
+
+// #endregion --- --- --- --- ---
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>welcome BomBa</h1>
+  <img
+    src="https://ik.imagekit.io/BomBa001/publicImages/BomBa-001.svg?updatedAt=1701827273385"
+    alt=""
+    srcset=""
+  />
+  <img
+    src="https://ik.imagekit.io/BomBa001/publicImages/BomBa-001.svg?updatedAt=1701827273385"
+    alt=""
+    srcset=""
+  />
+
+  <UContainer>
+    <UCard class="mt-10">
+      <template #header>
+        <div class="flex justify-between">
+          <h1>Welcome to Nuxt UI Starter</h1>
+          <ColorScheme
+            ><USelect
+              v-model="$colorMode.preference"
+              :options="['system', 'light', 'dark']"
+          /></ColorScheme>
+        </div>
+      </template>
+      <UButton
+        icon="i-heroicons-book-open"
+        to="https://ui.nuxt.com"
+        target="_blank"
+        >Open Nuxt UI Documentation</UButton
+      >
+    </UCard>
+  </UContainer>
+</template>
